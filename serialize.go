fff20956b6ca3af4e9ed1ddb150ffa876cf6f122
zheng peng <pengzheng@huishoubao.com.cn>
2023-11-27 11:20:09 +0800

    renam

--- serialize.go
+++ serialize.go
@@ -5,8 +5,8 @@ import (
 	"encoding/json"
 )
 
-// Struct2Bytes2StructJsonPacket 先将对象转换为byte，一系列处理后转换为新的对象，sdk调用场景，stream 的最外层,支持仅执行before 或者after
-func Struct2Bytes2StructJsonPacket(dataProvider interface{}, dataReceiver interface{}) (pack PackHandler) {
+// JsonMarshalUnMarshalPacket befor marshal,after Unmarshal，sdk调用场景，stream 的最外层,支持仅执行before 或者after
+func JsonMarshalUnMarshalPacket(dataProvider interface{}, dataReceiver interface{}) (pack PackHandler) {
 	return NewPackHandler(
 		func(ctx context.Context, input []byte) (out []byte, err error) {
 			if dataProvider == nil {
@@ -23,8 +23,8 @@ func Struct2Bytes2StructJsonPacket(dataProvider interface{}, dataReceiver interf
 	)
 }
 
-// Bytes2Stuct2BytesJsonPacket 先将byte转换为对象，一系列处理后转换为新的byte，server服务场景，stream 的最里层
-func Bytes2Stuct2BytesJsonPacket(dataReceiver interface{}, dataProvider interface{}) (pack PackHandler) {
+// JsonUnmarshalMarshalPacket before Unmarshal,after Marshal，server服务场景，stream 的最里层
+func JsonUnmarshalMarshalPacket(dataReceiver interface{}, dataProvider interface{}) (pack PackHandler) {
 	return NewPackHandler(
 		func(ctx context.Context, input []byte) (out []byte, err error) {
 			if dataReceiver == nil {
