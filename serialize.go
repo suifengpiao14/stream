5050dfccb9d4a02b861d8582695a806d5d965338
zheng peng <pengzheng@huishoubao.com.cn>
2023-11-27 11:14:54 +0800

    optimzie

--- serialize.go
+++ serialize.go
@@ -5,13 +5,19 @@ import (
 	"encoding/json"
 )
 
-// Struct2Bytes2StructJsonPacket 先将对象转换为byte，一系列处理后转换为新的对象，sdk调用场景，stream 的最外层
+// Struct2Bytes2StructJsonPacket 先将对象转换为byte，一系列处理后转换为新的对象，sdk调用场景，stream 的最外层,支持仅执行before 或者after
 func Struct2Bytes2StructJsonPacket(dataProvider interface{}, dataReceiver interface{}) (pack PackHandler) {
 	return NewPackHandler(
 		func(ctx context.Context, input []byte) (out []byte, err error) {
+			if dataProvider == nil {
+				return input, nil
+			}
 			return json.Marshal(dataProvider)
 		},
 		func(ctx context.Context, input []byte) (out []byte, err error) {
+			if dataReceiver == nil {
+				return input, nil
+			}
 			return nil, json.Unmarshal(input, dataReceiver)
 		},
 	)
@@ -21,9 +27,15 @@ func Struct2Bytes2StructJsonPacket(dataProvider interface{}, dataReceiver interf
 func Bytes2Stuct2BytesJsonPacket(dataReceiver interface{}, dataProvider interface{}) (pack PackHandler) {
 	return NewPackHandler(
 		func(ctx context.Context, input []byte) (out []byte, err error) {
+			if dataReceiver == nil {
+				return input, nil
+			}
 			return nil, json.Unmarshal(input, dataReceiver)
 		},
 		func(ctx context.Context, input []byte) (out []byte, err error) {
+			if dataProvider == nil {
+				return input, nil
+			}
 			return json.Marshal(dataProvider)
 		},
 	)
