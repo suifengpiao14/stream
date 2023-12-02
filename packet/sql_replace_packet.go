aa40cdf419e67b169b04941cf18e1a1518e795c6
pz <2912150017@qq.com>
2023-12-02 21:17:23 +0800

    a

--- packet/sql_replace_packet.go
+++ packet/sql_replace_packet.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 
 	"github.com/blastrain/vitess-sqlparser/sqlparser"
-	"github.com/spf13/cast"
 	"github.com/suifengpiao14/cudevent/cudeventimpl"
 	"github.com/suifengpiao14/sqlexec"
 	"github.com/suifengpiao14/sqlplus"
@@ -41,10 +40,6 @@ func (packet *_SQLReplacePacketHandler) Before(ctx context.Context, input []byte
 	if err != nil {
 		return ctx, nil, err
 	}
-	packet.sqlRawEvent = &cudeventimpl.SQLRawEvent{} // 重新初始化
-	packet.sqlRawEvent.SQL = sql
-	packet.sqlRawEvent.DB = packet.db
-	packet.sqlRawEvent.Stmt = stmt
 	switch stmt := stmt.(type) {
 	case *sqlparser.Update: // 更新类型，先查询更新前数据，并保存
 		selectSQL := sqlplus.ConvertUpdateToSelect(stmt)
@@ -57,31 +52,10 @@ func (packet *_SQLReplacePacketHandler) Before(ctx context.Context, input []byte
 			insertSql := sqlplus.ConvertUpdateToInsert(stmt)
 			//替换为insert语句后,重新设置事件内容
 			input = []byte(insertSql)
-			sql := string(input)
-			stmt, err := sqlparser.Parse(sql)
-			if err != nil {
-				return ctx, nil, err
-			}
-			packet.sqlRawEvent = &cudeventimpl.SQLRawEvent{} // 重新初始化
-			packet.sqlRawEvent.SQL = sql
-			packet.sqlRawEvent.DB = packet.db
-			packet.sqlRawEvent.Stmt = stmt
-
 		}
 	}
 	return ctx, input, nil
 }
 func (packet *_SQLReplacePacketHandler) After(ctx context.Context, input []byte) (newCtx context.Context, out []byte, err error) {
-	stmt := packet.sqlRawEvent.Stmt
-	switch stmt.(type) {
-	case *sqlparser.Insert:
-		packet.sqlRawEvent.LastInsertId = string(input)
-	case *sqlparser.Update:
-		packet.sqlRawEvent.RowsAffected = cast.ToInt64(string(input))
-	}
-	err = cudeventimpl.PublishSQLRawEvent(packet.sqlRawEvent)
-	if err != nil {
-		return ctx, nil, err
-	}
 	return ctx, input, nil
 }
